db962f8aa540becd8ba111ea0eb4f728d94224af
James Higgins <james.milan.higgins@gmail.com>
2017-07-24 14:28:09 -0700

    display total price

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -21,9 +21,7 @@ class Flight < ApplicationController
                                  headers: {"Content-Type" => "application/json"}
                                )
 
-JSON.parse(response)['trips']['tripOption'][0]['slice'][0]['segment'][0]['leg'][0]['mileage']
+     JSON.parse(response)
 
  end
 end
-
-#
