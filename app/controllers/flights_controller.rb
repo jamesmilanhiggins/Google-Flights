992224c3e704ade6d7ea47c95c278c7a87a7a385
James Higgins <James.Milan.Higgins@gmail.com>
2017-07-26 16:42:18 -0700

    add delete method and favicon

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,6 @@ class FlightsController < ApplicationController
 
   def index
     flight_object = Flight.new(params[:origin], params[:destination], params[:date])
-    # flight_object = Flight.new(flight_params)
     @flights = flight_object.get_flights
     @best_flight = Flight.get_best_deal
   end
--- app/controllers/user_flights_controller.rb
+++ app/controllers/user_flights_controller.rb
@@ -2,4 +2,10 @@ class UserFlightsController < ApplicationController
   def save_flight
     new_flight = current_user.user_flights.create(:price => params[:mpd])
   end
+
+  def destroy
+    savedflight = UserFlight.find(params[:id])
+    savedflight.destroy
+    redirect_to userflights_path
+  end
 end
