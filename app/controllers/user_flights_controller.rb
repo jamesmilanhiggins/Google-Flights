2be275e9dffd1460370824f1c070d52beb12759b
James Higgins <James.Milan.Higgins@gmail.com>
2017-07-26 14:06:17 -0700

    add user flight

--- app/controllers/user_flights_controller.rb
+++ app/controllers/user_flights_controller.rb
@@ -1,4 +1,22 @@
 class UserFlightsController < ApplicationController
-
+  # def create
+  #   @user = User.find(params[:user_id])
+  #   @user_flight = @user.user_flights.new(user_flight_params)
+  #   if @user_flight.save
+  #     redirect_to user_path(@user_flight.user)
+  #   else
+  #     render :new
+  #   end
+  # end
+  # def destroy
+  #   @user_flight = Review.find(params[:id])
+  #   @user_flight.destroy
+  #   redirect_to user_path(@user_flight.user)
+  # end
+  #
+  # private
+  # def user_flight_params
+  #   params.require(:user_flight).permit( :user_id)
+  # end
 
 end
