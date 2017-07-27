81113d95bff17da5bf1e7cc5906a2175bc1e5ee9
James Higgins <james.milan.higgins@gmail.com>
2017-07-27 11:46:21 -0700

    Add styling

--- app/controllers/user_flights_controller.rb
+++ app/controllers/user_flights_controller.rb
@@ -1,7 +1,6 @@
 class UserFlightsController < ApplicationController
   def save_flight
     new_flight = current_user.user_flights.create(mpd: params[:mpd], price: params[:price], airlineNumber: params[:airlineNumber], airlineCarrier: params[:airlineCarrier], departureTime: params[:departureTime], arribalTime: params[:arribalTime])
-    binding.pry
   end
 
   def destroy
